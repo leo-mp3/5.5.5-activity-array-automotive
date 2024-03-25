bc4864305e36e3b770349a902ca64a7b971fdca1
leo.mp3 <leonardohigaredav.3@gmail..com>
2024-03-25 13:09:39 -0700

    finished assignment

--- mercurySedan.js
+++ mercurySedan.js
@@ -5,6 +5,44 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
+class SuperVehicle extends VehicleModule {
+    constructor (maximumPassengers, passengers, numberOfWheels, fuel, scheduleService){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = maximumPassengers
+        this.passengers = passengers
+        this.numberOfWheels = numberOfWheels
+        this.maximumSpeed = maximumSpeed
+        this.fuel = fuel
+        this.scheduleService = scheduleService
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    scheduleService(){
+        if(mileage > 30000){
+            return this.timeForMaintenance = true
+            console.log(`Time For Maintenance`)
+        }
+    }
+    
+    loadPassenger(num){
+        let num = this.passengers
+        if(num > this.maximumPassengers){
+            console.log("Not enough space...")
+        }
+        else{
+            console('Drive Safe.')
+        }
+
+    }
+}
 
 //After you write the derived Car class, you should test it out.
 
@@ -12,9 +50,9 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+let myCar = new SuperVehicle('Mercury', 'A28', '2024', 'green', 90000 , 5, 6, 4, 150, 10, false)
 
-
-
+console.log(myCar)
 
 
 
